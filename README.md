c182f2f086bf0897cd029c11bf65202b1272cda0
Sunny <suk.jadoon@gmail.com>
2023-05-03 08:46:17 +0500

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+In order to start this project:
+
+1: Make sure that Node JS is installed in your machine. 
+   download link: https://nodejs.org/en/download
+2: Start command prompt in same directory (project directory)
+3: To install all the node modules, enter command: 
+    npm install
+4: After successfull installation of node modules, enter command:
+    npm start
+
+Now, the Invoice Generator App will be running in a while...
---
+++ src/pages/about/AboutScreen.js
@@ -0,0 +1,81 @@
+import React from "react";
+import PageTitle from "../../components/Common/PageTitle";
+
+function AboutScreen() {
+  return (
+    <div>
+      <div className="p-4">
+        <div className="bg-white rounded-xl p-3 font-title">
+          <PageTitle title="About Me" />
+          <div className="mt-4 mb-5 flex flex-row items-center">
+            <img
+              src="https://raw.githubusercontent.com/lwinmoepaing/lwinmoepaing/main/img/gitto.gif"
+              className="h-12 mr-3"
+              alt="Git"
+            />
+            <div>
+              <a
+                href="https://github.com/lwinmoepaing"
+                target={"_blank"}
+                className="underline cursor-pointer"
+                rel="noreferrer"
+              >
+                Hi, I'm Furqan
+              </a>
+              <h1> Free Invoice Maker for your business.</h1>
+            </div>
+          </div>
+
+          <PageTitle title="Invoice Maker" />
+          <div className="mt-2 pl-4 text-sm">
+            <ul class="list-disc">
+              <li> Can Easily Pre-Manage Your Products</li>
+              <li> Can Easily Pre-Manage Your Clients</li>
+              <li> Can Export PDF </li>
+              <li> Can Export Image </li>
+            </ul>
+          </div>
+          <div className="font-title mt-3 mb-5">
+            <div>
+              🤝 I’m looking for help with JS dev to build Many Free Softwares
+              to help people who can't afford !!
+            </div>
+            <div>
+              📫 How to reach me{" "}
+              <a
+                href="mailto:furqan123@gmail.com"
+                className="underline cursor-pointer"
+              >
+                furqan123@gmail.com
+              </a>{" "}
+              (or){" "}
+              <a
+                href="https://www.facebook.com/furqan/"
+                target={"_blank"}
+                className="underline cursor-pointer"
+                rel="noreferrer"
+              >
+                facebook
+              </a>
+            </div>
+            <div>
+              <span>🎁 </span>
+            </div>
+          </div>
+
+          <PageTitle title="Build With" />
+          <div className="mt-2 mb-5 pl-4 text-sm">
+            <ul class="list-disc">
+              <li> Framer Motion For each component Animation</li>
+              <li> Lottiefiles For Dashboard Widgets Icons</li>
+              <li> IndexedDB for Local Storage </li>
+              <li> ReactJS </li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AboutScreen;
